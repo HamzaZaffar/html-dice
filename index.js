e504f5e5fa4aaa92dcaec1836a459748a8cbb717
HamzaZaffar <hamzazafar5948@gmail.com>
2024-08-03 13:41:13 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+//random image 1//
+var randomNumber1=Math.floor(Math.random()*6+1);
+var randomDiceImage="dice"+randomNumber1+".png";
+var randomDiceSrc="images/"+randomDiceImage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomDiceSrc);
+
+//random image 2//
+var randomNumber2=Math.floor(Math.random()*6+1);
+var randomDiceSrc2="images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceSrc2);
+
+//heading//
+if (randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML="🚩Player 1 wins!";
+}
+else if (randomNumber2>randomNumber1) {
+    document.querySelector("h1").innerHTML="🚩Player 2 wins!";
+}
+else  {
+    document.querySelector("h1").innerHTML="Draw!";
+}
